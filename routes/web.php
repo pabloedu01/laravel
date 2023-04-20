e7ac11d4e6bb35a8e0871383f5fc1c64398d3a9b
pabloedu01 <pablo@pabloedu.com>
2023-04-20 20:16:23 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::phase('/', 'BlogController@HomePage');
-Route::phase('/about', 'BlogController@AboutPage');
-Route::phase('/contact', 'BlogController@ContactPage');
-Route::phase('/posts/{article}', 'BlogController@SingleArticle');
+Route::get('/', function () {
+    return view('welcome');
+});
