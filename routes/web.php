ae4e471d30ffcaa7a6eab52d97bfce5f59dd2d44
pabloedu01 <pablo@pabloedu.com>
2023-04-24 10:13:35 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/integracao/agenda/fila', 
+    [App\Http\Controllers\IntegracoesController::class, 'store']
+);
